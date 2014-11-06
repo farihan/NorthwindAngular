e6315a697ad9c7d8c07164353c6d27701393087f
Muhamad Farihan <farihan@gmail.com>
2014-11-06 22:30:20 +0800

    add generic repositories

---
+++ Hans.Angular/Hans.Northwind.Core/Repositories/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hans.Northwind.Core.Repositories
+{
+    public interface IRepository<TModel> where TModel : class
+    {
+        void Save(TModel instance);
+        void Update(TModel instance);
+        void Delete(TModel instance);
+        void SaveAsync(TModel instance);
+        void UpdateAsync(TModel instance);
+        void DeleteAsync(TModel instance);
+
+        IList<TModel> FindAll();
+        IList<TModel> FindAllBy(Expression<Func<TModel, bool>> where);
+        TModel FindOneBy(Expression<Func<TModel, bool>> where);
+
+        Task<IList<TModel>> FindAllAsync();
+        Task<IList<TModel>> FindAllByAsync(Expression<Func<TModel, bool>> where);
+        Task<TModel> FindOneByAsync(Expression<Func<TModel, bool>> where);
+    }
+}
---
+++ Hans.Angular/Hans.Northwind.Core/Repositories/Repository.cs
@@ -0,0 +1,96 @@
+﻿using Hans.Northwind.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hans.Northwind.Core.Repositories
+{
+    public class Repository<TModel> : IRepository<TModel> where TModel : class
+    {
+        private NORTHWNDContext Context { get; set; }
+
+        public Repository()
+        {
+            this.Context = new NORTHWNDContext();
+        }
+
+        public void Save(TModel instance)
+        {
+            Context.Set<TModel>().Add(instance);
+            Context.SaveChanges();
+        }
+
+        public void Update(TModel instance)
+        {
+            Context.Set<TModel>().Attach(instance);
+            Context.Entry(instance).State = System.Data.Entity.EntityState.Modified;
+            Context.SaveChanges();
+        }
+
+        public void Delete(TModel instance)
+        {
+            Context.Set<TModel>().Remove(instance);
+            Context.SaveChanges();
+        }
+
+        public void SaveAsync(TModel instance)
+        {
+            Context.Set<TModel>().Add(instance);
+            Context.SaveChangesAsync();
+        }
+
+        public void UpdateAsync(TModel instance)
+        {
+            Context.Set<TModel>().Attach(instance);
+            Context.Entry(instance).State = System.Data.Entity.EntityState.Modified;
+            Context.SaveChangesAsync();
+        }
+
+        public void DeleteAsync(TModel instance)
+        {
+            Context.Set<TModel>().Remove(instance);
+            Context.SaveChangesAsync();
+        }
+
+        public IList<TModel> FindAll()
+        {
+            return Context.Set<TModel>().ToList();
+        }
+
+        public IList<TModel> FindAllBy(System.Linq.Expressions.Expression<Func<TModel, bool>> where)
+        {
+            return Context.Set<TModel>().Where(where.Compile()).ToList();
+        }
+
+        public TModel FindOneBy(System.Linq.Expressions.Expression<Func<TModel, bool>> where)
+        {
+            return Context.Set<TModel>().FirstOrDefault(where.Compile());
+        }
+
+        public Task<IList<TModel>> FindAllAsync()
+        {
+            return Task.Run<IList<TModel>>(() =>
+            {
+                return Context.Set<TModel>().AsParallel().ToList();
+            });
+        }
+
+        public Task<IList<TModel>> FindAllByAsync(System.Linq.Expressions.Expression<Func<TModel, bool>> where)
+        {
+            return Task.Run<IList<TModel>>(() =>
+            {
+                return FindAllBy(where);
+            });
+        }
+
+        public Task<TModel> FindOneByAsync(System.Linq.Expressions.Expression<Func<TModel, bool>> where)
+        {
+            return Task.Run<TModel>(() =>
+            {
+                return FindOneBy(where);
+            });
+        }
+    }
+}
