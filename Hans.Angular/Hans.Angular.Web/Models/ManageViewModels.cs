10309fddf20143338cc9db1eb3ee5df9a9344d4a
Muhamad Farihan <farihan@gmail.com>
2014-11-14 20:22:44 +0800

    add in ManageViewModel

--- Hans.Angular/Hans.Angular.Web/Models/ManageViewModels.cs
+++ Hans.Angular/Hans.Angular.Web/Models/ManageViewModels.cs
@@ -5,13 +5,14 @@ using Microsoft.Owin.Security;
 
 namespace Hans.Angular.Web.Models
 {
-    public class IndexViewModel
+    public class IndexViewModel 
     {
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
+
     }
 
     public class ManageLoginsViewModel
