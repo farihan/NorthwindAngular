46230291dffe1e4ae85f573541cedac97c80a41d
Muhamad Farihan <farihan@gmail.com>
2015-03-05 10:09:38 +0800

    implement crud capabilities and validation

--- Hans.Angular/Hans.Angular.Web/Scripts/App/productController.js
+++ Hans.Angular/Hans.Angular.Web/Scripts/App/productController.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 angular.module('Northwind', ['ui.bootstrap']);
 
 angular.module('Northwind').controller('ProductController', function ($scope, $http, $modal) {
@@ -9,6 +9,8 @@ angular.module('Northwind').controller('ProductController', function ($scope, $h
         isAsc: true,
         totalItems: 0
     };
+
+    $scope.products = null;
 
     $scope.gridIndex = function (index) {
         return (index + 1) + (($scope.pagingInfo.page - 1) * $scope.pagingInfo.pageSize);
@@ -40,32 +42,36 @@ angular.module('Northwind').controller('ProductController', function ($scope, $h
             resolve: {
                 selectedID: function () {
                     return id;
+                },
+                selectedPagingInfo: function () {
+                    return $scope.pagingInfo;
                 }
             }
         });
 
-        modalInstance.result.then(function (selectedItem) {
-            $scope.selected = selectedItem;
+        modalInstance.result.then(function (refreshProducts) {
+            $scope.products = refreshProducts;
         }, function () {
             //$log.info('Modal dismissed at: ' + new Date());
         });
     };
 
     function loadTotalItems() {
-        $http.get('/api/product/getsize').success(function (data) {
+        $http.get('/api/product/getsize')
+        .success(function (data, status, headers, config) {
             $scope.pagingInfo.totalItems = data;
         })
-        .error(function () {
+        .error(function (data, status, headers, config) {
             $scope.error = "Error has occured!";
         });
     }
 
     function loadProducts() {
-        $scope.products = null;
-        $http.get('/api/product/getallby', { params: $scope.pagingInfo }).success(function (data) {
+        $http.get('/api/product/getallby', { params: $scope.pagingInfo })
+        .success(function (data, status, headers, config) {
             $scope.products = data;
         })
-        .error(function () {
+        .error(function (data, status, headers, config) {
             $scope.error = "Error has occured!";
         });
     }    
@@ -78,19 +84,90 @@ angular.module('Northwind').controller('ProductController', function ($scope, $h
     pageInit();
 });
 
-angular.module('Northwind').controller('ModalController', function ($scope, $modalInstance, $http, selectedID) {
+angular.module('Northwind').controller('ModalController', function ($scope, $modalInstance, $http, selectedID, selectedPagingInfo) {
     $scope.selectedID = selectedID;
+    $scope.selectedPagingInfo = selectedPagingInfo;
+
     $scope.ok = function () {
-        $modalInstance.close();
+        closeAndRefreshRepeater();
+        //$modalInstance.close();
     };
 
     $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
+        closeAndRefreshRepeater();
+        //$modalInstance.dismiss('cancel');
     };
-    
+
+    $scope.create = function (product) {
+        if ($scope.createForm.$valid) {
+            $http.post('/api/product/create', {
+                'ProductName' : product.ProductName,
+                'SupplierID' : product.SupplierID,
+                'CategoryID' : product.CategoryID,
+                'QuantityPerUnit' : product.QuantityPerUnit,
+                'UnitPrice' : product.UnitPrice,
+                'UnitsInStock' : product.UnitsInStock,
+                'UnitsOnOrder' : product.UnitsOnOrder,
+                'ReorderLevel' : product.ReorderLevel,
+                'Discontinued' : product.Discontinued
+            })
+            .success(function (data, status, headers, config) {
+                closeAndRefreshRepeater();
+            })
+            .error(function(data, status, headers, config) {
+                $scope.error = "Error has occured!";
+            });
+        }
+        else {
+        }
+    };
+
+    $scope.update = function (product) {        
+        if ($scope.editForm.$valid) {
+        $http({
+            method: 'PUT',
+            url: '/api/product/edit/' + product.ProductID ,
+            data: {
+                'ProductID': product.ProductID,
+                'ProductName': product.ProductName,
+                'SupplierID': product.SupplierID,
+                'CategoryID': product.CategoryID,
+                'QuantityPerUnit': product.QuantityPerUnit,
+                'UnitPrice': product.UnitPrice,
+                'UnitsInStock': product.UnitsInStock,
+                'UnitsOnOrder': product.UnitsOnOrder,
+                'ReorderLevel': product.ReorderLevel,
+                'Discontinued': product.Discontinued
+            },
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .success(function (data, status, headers, config) {
+                closeAndRefreshRepeater();
+            })
+            .error(function (data, status, headers, config) {
+                $scope.error = "Error has occured!";
+            });
+        }
+        else {
+        }
+    };
+
+    $scope.delete = function (id) {
+        $http.delete('/api/product/delete', {
+            params: { 'id': id }
+        })
+        .success(function (data, status, headers, config) {
+            closeAndRefreshRepeater();
+        })
+        .error(function (data, status, headers, config) {
+            $scope.error = "Error has occured!";
+        });
+    };
+
     function getAvailableSuppliers() {
         $scope.availableSuppliers = [];
-        $http.get('/api/supplier/getall').success(function (data) {
+        $http.get('/api/supplier/getall')
+        .success(function (data, status, headers, config) {
             $.each(data, function (index, item) {
                 $scope.availableSuppliers.push({ ID: item.SupplierID, Name: item.ContactName + ' (' + item.CompanyName + ')' });
             });
@@ -102,24 +179,52 @@ angular.module('Northwind').controller('ModalController', function ($scope, $mod
 
     function getAvailableCategories() {
         $scope.availableCategories = [];
-        $http.get('/api/category/getall').success(function (data) {
+        $http.get('/api/category/getall')
+        .success(function (data, status, headers, config) {
             $.each(data, function (index, item) {
                 $scope.availableCategories.push({ ID: item.CategoryID, Name: item.CategoryName });
             });
         })
-        .error(function () {
+        .error(function (data, status, headers, config) {
             $scope.error = "Error has occured!";
         });
     }
 
     function loadProduct() {
         $scope.product = null;
-        $http.get('/api/product/get', { params: { id: $scope.selectedID } }).success(function (data) {
+        $http.get('/api/product/get', { params: { id: $scope.selectedID } })
+        .success(function (data, status, headers, config) {
             $scope.product = data;
         })
-        .error(function () {
+        .error(function (data, status, headers, config) {
             $scope.error = "Error has occured!";
         });
+    }
+
+    function loadTotalItems() {
+        $http.get('/api/product/getsize')
+        .success(function (data, status, headers, config) {
+            $scope.selectedPagingInfo.totalItems = data;
+        })
+        .error(function (data, status, headers, config) {
+            $scope.error = "Error has occured!";
+        });
+    }
+
+    function loadProducts() {
+        $http.get('/api/product/getallby', { params: $scope.selectedPagingInfo })
+        .success(function (data, status, headers, config) {
+            $scope.refreshProducts = data;
+            $modalInstance.close(data);
+        })
+        .error(function (data, status, headers, config) {
+            $scope.error = "Error has occured!";
+        });
+    }
+
+    function closeAndRefreshRepeater() {
+        loadTotalItems();
+        loadProducts();
     }
 
     function pageInit() {
